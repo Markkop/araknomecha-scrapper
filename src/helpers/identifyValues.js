76297b6f6b2f318316ff9e195db7ad0b22f6f4f3
Mark Kop <mk.marcelo.mk@gmail.com>
2020-09-19 18:11:10 -0300

    feat: migrate identify values from corvo-astral

---
+++ src/helpers/identifyValues.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-unused-vars */
+import itemTypeData from '../../data/raw/equipmentItemTypes.json'
+import itemsData from '../../data/raw/items.json'
+import storeData from './storeData'
+
+/**
+ * Identify actions from all equipment effects.
+ *
+ * @returns {object}
+ */
+function identifyActionValues () {
+  return itemsData.reduce((actionValues, equip) => {
+    const effects = equip.effects
+    const equipActions = effects.reduce((equipActions, effect) => {
+      if (actionValues[effect.action]) {
+        return equipActions
+      }
+
+      return {
+        ...equipActions,
+        [effect.action]: effect.descriptions[0]
+          .replace('[el1]', 'fogo')
+          .replace('[el2]', 'agua')
+          .replace('[el3]', 'terra')
+          .replace('[el4]', 'ar')
+          .replace(/[0-9]| de/g, '')
+          .replace(/\s\s+/g, ' ')
+          .trim()
+      }
+    }, {})
+    return { ...actionValues, ...equipActions }
+  }, {})
+}
+
+/**
+ * Identify actions from all equipment effects.
+ *
+ * @returns {object}
+ */
+function identifyUseEffectsActionValues () {
+  return itemsData.reduce((actionValues, equip) => {
+    const useEffects = equip.useEffects
+    const equipActions = useEffects.reduce((equipActions, useEffect) => {
+      if (actionValues[useEffect.action]) {
+        return equipActions
+      }
+
+      return {
+        ...equipActions,
+        [useEffect.action]: useEffect.descriptions[0]
+          .replace('[el1]', 'fogo')
+          .replace('[el2]', 'agua')
+          .replace('[el3]', 'terra')
+          .replace('[el4]', 'ar')
+          .replace(/[0-9]| de/g, '')
+          .replace(/\s\s+/g, ' ')
+          .trim()
+      }
+    }, {})
+    return { ...actionValues, ...equipActions }
+  }, {})
+}
+
+/**
+ * Get all types from equipment with an example equipment name.
+ *
+ * @returns {object}
+ */
+function getAllTypes () {
+  return itemsData.reduce((types, equip) => {
+    const currentName = equip.title
+    const currentType = equip.type
+    if (types[currentType]) {
+      return types
+    }
+    return { ...types, [currentType]: currentName }
+  }
+  , {})
+}
+
+/**
+ * Build an object with equipment type id and title.
+ *
+ * @returns {object}
+ */
+function getEquipmentItemTypes () {
+  return itemTypeData.reduce((equipTypes, type) => {
+    return {
+      ...equipTypes,
+      [type.definition.id]: type.title
+    }
+  }, {})
+}
+
+storeData(getEquipmentItemTypes(), 'data/generated/equipTypes.json')
