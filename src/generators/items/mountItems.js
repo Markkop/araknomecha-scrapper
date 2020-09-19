4d7f107091b61f99ada86e3c8a26377e8a499c9e
Mark Kop <mk.marcelo.mk@gmail.com>
2020-09-19 11:17:41 -0300

    feat: enrich sublimation data inside item list

--- src/generators/items/mountItems.js
+++ src/generators/items/mountItems.js
@@ -1,13 +1,28 @@
 import itemsData from '../../../data/raw/items'
 import { storeData } from '../../helpers'
 import { parseEffect } from '../../parsers/parseEffect'
+import epic from '../../../data/raw/sublimations/epic.json'
+import relic from '../../../data/raw/sublimations/relic.json'
+import normal from '../../../data/raw/sublimations/normal.json'
 import fs from 'fs'
+
+const sublimations = [...epic, ...relic, ...normal]
 
 const equipmentList = {
   en: JSON.parse(fs.readFileSync('data/raw/method/methodEquipment_en.json')),
   es: JSON.parse(fs.readFileSync('data/raw/method/methodEquipment_es.json')),
   fr: JSON.parse(fs.readFileSync('data/raw/method/methodEquipment_fr.json')),
   pt: JSON.parse(fs.readFileSync('data/raw/method/methodEquipment_pt.json'))
+}
+
+/**
+ * Check if the given object has any truthy property.
+ *
+ * @param {object} obj
+ * @returns {boolean}
+ */
+function hasInfo (obj) {
+  return Object.keys(obj).some(key => Boolean(obj[key]))
 }
 
 /**
@@ -25,7 +40,7 @@ function mountItems () {
     mappedItem.id = itemDefinition.id
     mappedItem.title = itemData.title
     mappedItem.description = itemData.description
-    mappedItem.level = itemDefinition.itemLevel
+    mappedItem.level = itemLevel
     mappedItem.useEffects = useEffects
     mappedItem.equipEffects = equipEffects
     mappedItem.useParameters = {
@@ -34,9 +49,35 @@ function mountItems () {
       useCostWp: useParameters.useCostWp
     }
     mappedItem.imageId = itemDefinition.graphicParameters.gfxId
-    mappedItem.itemTypeId = itemDefinition.itemTypeId
-    mappedItem.itemSetId = itemDefinition.itemSetId
-    mappedItem.rarity = itemDefinition.rarity
+    mappedItem.itemTypeId = itemDefinition.baseParameters.itemTypeId
+    mappedItem.itemSetId = itemDefinition.baseParameters.itemSetId
+    mappedItem.rarity = itemDefinition.baseParameters.rarity
+
+    // Enrich sublimations information
+    const sublimation = sublimations.find(subli => subli.name.toLowerCase() === itemData.title.pt.toLowerCase().trim())
+    if (!sublimation && Array.isArray(itemDefinition.sublimationParameters && itemDefinition.sublimationParameters.slotColorPattern)) {
+      console.log('An item sublimation was not found in sublimation list:', { name: itemData.title.en, id: itemDefinition.id })
+    }
+
+    if (sublimation) {
+      mappedItem.sublimation = itemDefinition.sublimationParameters
+      mappedItem.sublimation.effects = sublimation.effects
+      mappedItem.sublimation.source = {}
+      if (hasInfo(sublimation.source.drop.monster)) {
+        mappedItem.sublimation.source.drop = sublimation.source.drop
+      }
+      if (hasInfo(sublimation.source.chest)) {
+        mappedItem.sublimation.source.chest = sublimation.source.chest
+      }
+      if (hasInfo(sublimation.source.craft)) {
+        mappedItem.sublimation.source.craft = sublimation.source.craft
+      }
+      if (hasInfo(sublimation.source.quest)) {
+        mappedItem.sublimation.source.quest = sublimation.source.quest
+      }
+    }
+
+    // Enrich conditions information
     mappedItem.conditions = {}
     mappedItem.conditions.description = {}
     const langs = ['en', 'pt', 'fr', 'es']
